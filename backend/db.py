d91035f3e1e49f04d7599d22333a8640decd8de8
nikitakuvsh <world_fre@mail.ru>
2025-06-26 01:56:12 +0300

    update bd link

--- backend/db.py
+++ backend/db.py
@@ -3,9 +3,10 @@ from psycopg2.extras import RealDictCursor
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="roulette_db",
-        user="postgres",
-        password="2gecf232gecf2",
+        host="dpg-d1e7rveuk2gs73adr1p0-a.oregon-postgres.render.com",
+        port=5432,
+        database="tg_miniapp_roullete_bd",
+        user="tg_miniapp_roullete_bd_user",
+        password="nyilaFOrUBtbQf3ybR3jRczDNwhB04PZ",
         cursor_factory=RealDictCursor
     )
